9e7e87dfe4ad19f004d551c7c59781dd28f1adf1
Samuel Meuli <me@samuelmeuli.com>
2018-10-28 20:52:04 +0100

    Convert to PureComponent

--- src/Magnifier.js
+++ src/Magnifier.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import throttle from 'lodash.throttle';
 
@@ -46,7 +46,7 @@ const defaultProps = {
 	mgTouchOffsetY: -50
 };
 
-export default class Magnifier extends Component {
+export default class Magnifier extends PureComponent {
 	constructor(props) {
 		super(props);
 
